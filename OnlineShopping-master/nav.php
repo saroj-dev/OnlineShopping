09d2d9a4003294e23c50e56186c5723129a5878e
Aashish Panthi <60884239+aashishpanthi@users.noreply.github.com>
2020-09-24 17:56:52 +0545

    admin files

---
+++ OnlineShopping-master/nav.php
@@ -0,0 +1,88 @@
+<?php
+include './connection.php';
+
+?>
+
+
+<div class="nav">
+    <div class="open">
+        <!-- svg -->
+      <svg  id="svg_icon_bar "aria-hidden="true" focusable="false" data-prefix="fas"   viewBox="0 0 448 512">
+          <path  d="M16 132h416c8.837 0 16-7.163 16-16V76c0-8.837-7.163-16-16-16H16C7.163 60 0 67.163 0 76v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16z">
+          </path>
+      </svg>
+        <!-- svg -->
+    </div>
+   <!-- right slide  -->
+  <div class="rightnav">
+      <div class="myOrder">
+          <a href="#Myorder">
+              <i style="color:#e8e8e8;"class="fas fa-shopping-cart"></i>
+          </a>
+      </div>
+      <div class="search">
+          <div class="search__con">
+          <input type="text" name="search_query" id="search_id" list='search_options' autocomplete="off">
+          <i style="color:#e8e8e8;"class="fas fa-search"></i>
+      </div>
+      <datalist id='search_options'>
+			<?php 
+				$sqlQuery = "SELECT laptopName FROM `laptop`;";
+	            $resultOfOptions = mysqli_query($connection,$sqlQuery);
+	            $OptionsRow = mysqli_num_rows($resultOfOptions);
+	            if($OptionsRow > 0){
+	                while($options = mysqli_fetch_assoc($resultOfOptions)){
+	                    echo '<option value='.$options['laptopName'].'> </option>';
+	                }
+	            }
+			?>
+		</datalist>
+      </div>
+      <div class="user_account">
+          <a href="#Profile">
+              <i style="color:#e8e8e8;"class="fas fa-user-circle"></i>
+          </a>
+      </div>
+  </div>
+</div>
+   <!-- right slide  -->
+    <div class="side_slide_nav">
+
+        <ul>
+              <div class="shop">
+                  <h3>
+                      shop by category &nbsp; &nbsp; &nbsp;  &#8620;
+                  </h3>
+              </div>
+
+                  <a href="#Laptop" class="cat nav_ko">
+                      <li>
+                          <i style="color:#e8e8e8;"class="fas fa-laptop"></i>    Laptop
+                      </li>
+                    </a>
+                 
+                    <a href="#Mobile" class="cat nav_ko">
+                      <li>
+                          <i style="color:#e8e8e8;"class="fas fa-mobile"></i>
+                          Mobile
+                      </li>
+                    </a>
+
+             
+                    <a href="#Myorder" class="nav_ko">
+                      <li><i style="color:#e8e8e8;"class="fas fa-shopping-cart"></i>
+                          My Orders
+                      </li>
+                  </a>
+
+                  <a href="#Profile" class="nav_ko">
+                      <li>
+                          <i style="color:#e8e8e8;"class="fas fa-user-circle"></i>
+                          Profile
+
+                      </li>
+                    </a>
+        </ul>
+    </div>
+
+  
